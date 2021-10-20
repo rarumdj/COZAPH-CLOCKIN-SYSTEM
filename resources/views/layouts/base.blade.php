a2de3a6dd06e8c2679a99d88a02e59b2655126f8
David J <davemoses3@gmail.com>
2021-10-20 12:38:09 +0100

    update

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -323,7 +323,8 @@
                         </ul>
                     </div>
                     <div class="col-sm-6 text-center text-sm-left mt-3 mt-sm-0">
-                        <small class="text-muted d-block">Copyright © 2019 <a>COZA</a>. All rights reserved</small>
+                        <small class="text-muted d-block">Copyright © {{ date('Y') }} <a>COZA</a>. All rights
+                            reserved</small>
                         {{-- <small class="text-gray mt-2">Handcrafted With <i class="mdi mdi-heart text-danger"></i></small> --}}
                     </div>
                 </div>
