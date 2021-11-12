ad6cbe1ee2054aa95047a73ef2dd348ff984ad5b
David J <davemoses3@gmail.com>
2021-11-12 20:04:02 +0100

    update

--- resources/views/emails/clockin-mail.blade.php
+++ resources/views/emails/clockin-mail.blade.php
@@ -1,9 +1,14 @@
 @component('mail::message')
 <div class="mb-5 w-100 text-center">
-    <h1>Dear, {{ $body['name'] }} ({{ $body['user_id'] }})</h1>
+    <h1>Dear, {{ $body['name'] }}</h1>
     <p>Clockin activity {{ $body['date'] }}.</p>
 </div>
 
+<div class="p-5 text-center">
+    @component('mail::panel')
+    <p>User ID: {{ $body['user_id'] }}</p>
+    @endcomponent
+</div>
 <div class="p-5 text-center">
     @component('mail::panel')
     <p>Call Time: {{ $body['calltime'] }}</p>
